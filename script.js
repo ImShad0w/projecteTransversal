64a9a3f5b3e28bc5219dd9a7489e54aa3b9bd48d
shad0w <kirillpashnin.dev@gmail.com>
2025-09-16 12:28:13 +0200

    Added function to show one question at a time and responses

--- script.js
+++ script.js
@@ -3,30 +3,61 @@ import data from "./data.js";
 
 //Get the base fields for rendering
 const container = document.querySelector(".container");
+const preg = document.querySelector(".pregunta");
+const respostes = document.querySelector(".respostes");
 const startGame = document.getElementById("startGame");
 
+//Game state object with answers
+const gameState = {
+  gameOver: false,
+  currQuestion: 0,
+  userAnswers: [],
+};
+//let gameOver = true;
+//let currQuestion = 0;
+//let correctAnswers = 0;
 //Get the questions
 let preguntes = data["preguntes"];
 
 //TODO: Create a way for rendering one question and it's responses at a time
 
 startGame.addEventListener("click", () => {
-  preguntes.forEach(pregunta => {
-    const preg = document.createElement("p");
-    const resposta1 = document.createElement("p");
-    const resposta2 = document.createElement("p");
-    const resposta3 = document.createElement("p");
-    const resposta4 = document.createElement("p");
-    preg.textContent = pregunta["pregunta"];
-    resposta1.textContent = pregunta["respostes"][0];
-    resposta2.textContent = pregunta["respostes"][1];
-    resposta3.textContent = pregunta["respostes"][2];
-    resposta4.textContent = pregunta["respostes"][3];
-    container.appendChild(preg);
-    container.appendChild(resposta1);
-    container.appendChild(resposta2);
-    container.appendChild(resposta3);
-    container.appendChild(resposta4);
-  })
+  gameStart();
 })
+
+function gameStart() {
+  gameState.gameOver = false;
+  generateQuestion();
+}
+
+function generateQuestion() {
+  //Clear out the fields before creating a question
+  preg.innerHTML = "";
+  respostes.innerHTML = "";
+  //Get the current question
+  const pregunta = preguntes[gameState.currQuestion].pregunta;
+  const preguntaID = preguntes[gameState.currQuestion].id;
+  const answers = preguntes[gameState.currQuestion].respostes;
+  //Create the text for the question
+  const question = document.createElement("p");
+  question.textContent = pregunta;
+  preg.appendChild(question);
+  //Append the question to the container
+  container.appendChild(preg);
+  answers.forEach(resposta => {
+    const resp = document.createElement("div");
+    const text = document.createElement("p");
+    text.textContent = resposta;
+    //Add the class
+    resp.classList.add("resposta")
+    resp.appendChild(text);
+    respostes.appendChild(resp);
+    container.appendChild(respostes);
+    resp.addEventListener("click", () => {
+      gameState.currQuestion++;
+      generateQuestion();
+      gameState.userAnswers.push({ id: preguntaID, resposta: answers.indexOf(resposta) });
+    })
+  })
+}
 
